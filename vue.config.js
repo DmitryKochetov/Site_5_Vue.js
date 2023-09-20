40bdac009de2a7d4b2841a4907c42186f1e0db3b
Dmitry <kochetov.dmitry@mail.ru>
2023-09-20 16:48:57 +0300

    mini css plugin added

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
     ? '/Site_5_Vue.js/'
-    : '/'
+    : '/',
+    configureWebpack: {
+      plugins: [new MiniCssExtractPlugin()],
+    }
 })
